a010452ce6f6044bcf4135d8fe412eb068e41847
Anki Satya <95831101+Ansaku@users.noreply.github.com>
2022-01-19 21:18:38 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,30 +9,7 @@ APP_ID = int(os.environ['APP_ID'])
 BOT_TOKEN = os.environ['BOT_TOKEN']
 downloads = './downloads/{}/'
 
-#Button
-START_BUTTONS=[
-    [
-        InlineKeyboardButton('Source', url='https://github.com/Ansaku/TikTok-DL-Bot'),
-        InlineKeyboardButton('Update Channel', url='https://t.me/ansakubotchannel'),
-    ],
-    [InlineKeyboardButton('Author', url='https://t.me/AnkiSatya')],
-]
-
-DL_BUTTONS=[
-    [
-        InlineKeyboardButton('No Watermark', callback_data='nowm'),
-        InlineKeyboardButton('Watermark', callback_data='wm'),
-    ],
-    [InlineKeyboardButton('Audio', callback_data='audio')],
-]
-
-
-# Running bot
-xbot = Client('TikTokDL', api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-
-# Helpers
-# Thanks to FridayUB
+# Helper
 async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
   args = shlex.split(cmd)
   process = await asyncio.create_subprocess_exec(
@@ -46,10 +23,96 @@ async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
       process.pid,
   )
 
+# Button
+START_TEXT = """
+Hai {}, Saya adalah TikTok-DL-Bot. Anda dapat mengunduh video/audio TikTok menggunakan bot ini. Kirimkan saya tautan tiktok yang ingin di unduh.
+
+Dibuat oleh @AnkiSatya
+"""
+HELP_TEXT = """
+- Kirimkan saya tautan tiktok
+- Pilih opsi yang anda inginkan
+- Saya akan mengunduhnya
+- Dan saya akan mengirim video/audio sesuai yang anda kirim
+
+Dibuat oleh @AnkiSatya
+"""
+ABOUT_TEXT = """
+- **Bot :** `TikTok DL Bot`
+- **Pembuat :** [𝔸𝕟𝕜𝕚 𝕊𝕒𝕥𝕪𝕒](https://t.me/AnkiSatya)
+- **Channel :** [𝕬𝖓𝖘𝖆𝖐𝖚 𝕭𝖔𝖙 𝕮𝖍𝖆𝖓𝖓𝖊𝖑](https://t.me/ansakubotchannel)
+- **Repo :** [Github](https://github.com/Ansaku/TikTok-DL-Bot)
+- **Donasi :** [Saweria](https://saweria.co/ansaku)
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Channel', url='https://t.me/ansakubotchannel'),
+        InlineKeyboardButton('Author', url='https://t.me/AnkiSatya')
+        ],[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+
+DL_BUTTONS=[
+    [
+        InlineKeyboardButton('No Watermark', callback_data='nowm'),
+        InlineKeyboardButton('Watermark', callback_data='wm'),
+    ],
+    [InlineKeyboardButton('Audio', callback_data='audio')],
+]
+
+
+# Running bot
+xbot = Client('TikTok-DL-Bot', api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+
 # Start
-@xbot.on_message(filters.command('start') & filters.private)
-async def _start(bot, update):
-  await update.reply_text(f"Hai... ðŸ‘‹\nSaya TikTok-DL-Bot!\nAnda dapat mengunduh video/audio TikTok menggunakan bot ini. Kiriman saya tautan\nDibuat oleh @AnkiSatya", True, reply_markup=InlineKeyboardMarkup(START_BUTTONS))
+@xbot.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            reply_markup=START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            reply_markup=HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            reply_markup=ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.message.delete()
+
+@xbot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTONS
+    )
 
 # Downloader for tiktok
 @xbot.on_message(filters.regex(pattern='.*http.*') & filters.private)
